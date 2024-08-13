edbc7c45705ec15c587d3c674cb3586c26f46af9
ConvolutedDog <102723346+ConvolutedDog@users.noreply.github.com>
2024-08-13 13:12:09 +0800

    Create Paper-Figures-Pre-Provided.sh

---
+++ Paper-Figures-Pre-Provided.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python3 fig_11_for_pre_provided_A100_results.py
+python3 fig_8a_for_pre_provided_GV100_results.py
+python3 fig_2a_for_pre_provided_GV100_results.py
+python3 fig_13a_for_pre_provided_GV100_results.py
+python3 fig_8b_for_pre_provided_GV100_results.py
+python3 fig_2b_for_pre_provided_GV100_results.py
+python3 fig_13b_for_pre_provided_GV100_results.py
+python3 fig_8c_for_pre_provided_GV100_results.py
+python3 fig_2c_for_pre_provided_GV100_results.py
+python3 fig_13c_for_pre_provided_GV100_results.py
+python3 fig_2d_for_pre_provided_GV100_results.py
+python3 fig_14_for_pre_provided_GV100_results.py
